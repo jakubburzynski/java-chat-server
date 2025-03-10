f132ea1c66abf07c60c7eb1cf758fb6d8402814d
jakubburzynski <33811303+jakubburzynski@users.noreply.github.com>
2025-03-10 22:26:09 +0100

    setup simple socket server

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
+    private static final int PORT = 17777;
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            System.out.println("server listening on port: " + PORT);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new ChatClient(socket).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
